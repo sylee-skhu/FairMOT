b23499901b7dbef85a9ddafcede5b1f759ba02d3
Sangyun <sylee@skhu.ac.kr>
2022-08-17 23:01:00 +0900

    minor fix

--- src/gen_labels_crowd_id.py
+++ src/gen_labels_crowd_id.py
@@ -49,12 +49,12 @@ def gen_labels_crowd(data_root, label_root, ann_root):
 
 
 if __name__ == '__main__':
-    data_val = '/data/yfzhang/MOT/JDE/crowdhuman/images/val'
-    label_val = '/data/yfzhang/MOT/JDE/crowdhuman/labels_with_ids/val'
-    ann_val = '/data/yfzhang/MOT/JDE/crowdhuman/annotation_val.odgt'
-    data_train = '/data/yfzhang/MOT/JDE/crowdhuman/images/train'
-    label_train = '/data/yfzhang/MOT/JDE/crowdhuman/labels_with_ids/train'
-    ann_train = '/data/yfzhang/MOT/JDE/crowdhuman/annotation_train.odgt'
+    data_val = '/home/sylee/Dataset/crowdhuman/images/val'
+    label_val = '/home/sylee/Dataset/crowdhuman/labels_with_ids/val'
+    ann_val = '/home/sylee/Dataset/crowdhuman/annotation_val.odgt'
+    data_train = '/home/sylee/Dataset/crowdhuman/images/train'
+    label_train = '/home/sylee/Dataset/crowdhuman/labels_with_ids/train'
+    ann_train = '/home/sylee/Dataset/crowdhuman/annotation_train.odgt'
     gen_labels_crowd(data_train, label_train, ann_train)
     gen_labels_crowd(data_val, label_val, ann_val)
 
